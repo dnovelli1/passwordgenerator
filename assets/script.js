3c410274478c943d9d0371fc17c930e7f073896e
Jake Novelli <jakenovelli11@gmail.com>
2021-03-23 17:43:45 -0700

    added variables for each symbol and tested variables in a random generator

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,32 @@
+var passUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var passLower = "abcdefghijklmnopqrstuvwxyz";
+
+var passNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var passChar = "!#$%&'()*+,-./:;<=>?@[\}^_`{|}~";
+
+
+
+function randomLower() {
+
+  return passLower[Math.floor(Math.random()*passLower.length)];
+
+}
+
+
+function randomNumber() {
+  
+  return passNumber[Math.floor(Math.random()*passNumber.length)];
+
+}
+
+console.log(randomNumber());
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
