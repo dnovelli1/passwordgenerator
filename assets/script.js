820ee93fe8ac82674b6da5222aa91d170334a2af
Jake Novelli <jakenovelli11@gmail.com>
2021-03-26 15:43:13 -0700

    condensed each if statement into 4 statements, to make the code less dry. Also changed each array into a string

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-// // // Assignment Code
-var useUpperCase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var useLowerCase = [ "a", "b", "c", "d" , "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var useNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var useSymbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", "<", "=", ">", "?", "@"];
+// Assignment Code
+var useUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var useLowerCase = "abcdefghijklmnopqrstuvwxyz";
+var useNumbers = "0123456789";
+var useSymbols = "!#$%&(,)*+-./:<=>?@";
 var userMinChar = 8;
 var userMaxChar = 128;
 
@@ -26,10 +26,11 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-      // This will prompt the user to enter the number of characters they want with restrictions
+    // This will prompt the user to enter the number of characters they want with restrictions
     userNumber = prompt("How many characters is your password?");
     var userWants = "";
-      // If the number the user chose is less than 8 or greater than 128, the alert will pop up leading them back to the beginning
+    var allUserWants = "";
+    // If the number the user chose is less than 8 or greater than 128, the alert will pop up leading them back to the beginning
     if (userNumber >= userMinChar && userNumber <= userMaxChar) {
       wantUpper = confirm("Would you like to use Capitol Letters?");
       wantLower = confirm("Would you like to use Lower Case Letters?");
@@ -42,50 +43,17 @@ function generatePassword() {
     };
     // Start sequence of possible outcomes from the user by concatinating into allUserWants.
     // Try to figure out if there is a way to loop all of the possible outcomes...?
-    if (wantUpper && wantLower && wantSymbols && wantNumbers) {
-      allUserWants = useUpperCase.concat(useLowerCase, useSymbols, useNumbers);
+    if (wantUpper === true) {
+      allUserWants = allUserWants.concat(useUpperCase);
     }
-    else if (wantUpper && wantLower && wantSymbols) {
-      allUserWants = useUpperCase.concat(useLowerCase, useSymbols);
+    if (wantLower === true) {
+      allUserWants = allUserWants.concat(useLowerCase);
     }
-    else if (wantUpper && wantNumbers && wantSymbols) {
-      allUserWants = useUpperCase.concat(useNumbers, useSymbols);
+    if (wantSymbols === true) {
+      allUserWants = allUserWants.concat(useSymbols);
     }
-    else if (wantLower && wantNumbers && wantSymbols) {
-      allUserWants = useLowerCase.concat(useNumbers, useSymbols)
-    }
-    else if (wantLower && wantUpper && wantNumbers) {
-      allUserWants = useLowerCase.concat(useUpperCase, useNumbers)
-    }
-    else if (wantUpper && wantLower) {
-      allUserWants = useUpperCase.concat(useLowerCase)
-    }
-    else if (wantUpper && wantNumbers) {
-      allUserWants = useUpperCase.concat(useNumbers)
-    }
-    else if (wantUpper && wantSymbols) {
-      allUserWants = useUpperCase.concat(useSymbols)
-    }
-    else if (wantLower && wantNumbers) {
-      allUserWants = useLowerCase.concat(useNumbers)
-    }
-    else if (wantLower && wantSymbols) {
-      allUserWants = useLowerCase.concat(useSymbols)
-    }
-    else if (wantNumbers && wantSymbols) {
-      allUserWants = useNumbers.concat(useSymbols)
-    }
-    else if (wantUpper) {
-      allUserWants = useUpperCase;
-    }
-    else if (wantLower) {
-      allUserWants = useLowerCase;
-    }
-    else if (wantNumbers) {
-      allUserWants = useNumbers;
-    }
-    else if (wantSymbols) {
-      allUserWants = useSymbols;
+    if (wantNumbers === true) {
+      allUserWants = allUserWants.concat(useNumbers);
     }
     // Randomized generator for allUserWants
     for (var i = 0; i < userNumber; i++) {
@@ -93,4 +61,4 @@ function generatePassword() {
     }
     return userWants;
   }
-// Console.log the for loop provides undefined in the text box but provides random concat of choices.
+    // Console.log the for loop provides undefined in the text box but provides random concat of choices.
